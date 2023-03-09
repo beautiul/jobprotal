0ac9ab8d917e939aa26007d669c9e2e42cdfc069
beautiul <127199579+beautiul@users.noreply.github.com>
2023-03-09 20:38:38 +0530

    Add files via upload

--- db.php
+++ db.php
@@ -1,7 +1,8 @@
 <?php
 /* Database connection settings */
-$host = 'localhost';  //default host on your local computer
-$user = 'root';       //default Username of MySql
-$pass = ''; //edit this value with your db password
+$host = 'database-1.cfclhczdhrtn.us-east-1.rds.amazonaws.com';  //default host on your local computer
+$user = 'admin';       //default Username of MySql
+$pass = 'admin123'; //edit this value with your db password
 $db = 'accounts';     //edit this value with your db name
+
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
